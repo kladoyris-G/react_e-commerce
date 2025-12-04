0703ed6729332c9b63634b17b9e5fdd62e89d8fa
giorgos <gkladouris@taxaki.com>
2025-12-04 17:11:48 +0200

    first burger menu ui

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,27 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { FaShoppingCart, FaUserCircle } from "react-icons/fa";
+import { FaShoppingCart, FaUserCircle, FaCaretDown } from "react-icons/fa";
 import styles from "./Header.module.css";
 import Home from "../../pages/home/Home";
+import { slide as Menu } from "react-burger-menu";
+import "./Burger.css";
 
 const Navbar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const closeMenu = () => setIsOpen(false);
+
   return (
     <nav className={styles.navbar}>
+      {/* Mobile Burger Icon */}
+      <Menu isOpen={isOpen} onStateChange={(state) => setIsOpen(state.isOpen)}>
+        <Link onClick={closeMenu} to="/shop/category1">
+          Category 1
+        </Link>
+        <Link onClick={closeMenu} to="/shop/category2">
+          Category 2
+        </Link>
+        <Link onClick={closeMenu} to="/shop/category3">
+          Category 3
+        </Link>
+        <Link onClick={closeMenu} to="/on-sale">
+          On Sale
+        </Link>
+        <Link onClick={closeMenu} to="/new-arrivals">
+          New Arrivals
+        </Link>
+        <Link onClick={closeMenu} to="/brands">
+          Brands
+        </Link>
+      </Menu>
+
       {/* Logo */}
       <h1 className={styles.logo}>
         <Link to={Home.route}>SHOP.CO</Link>
       </h1>
 
-      {/* Menu */}
+      {/* Desktop Menu */}
       <ul className={styles.menu}>
         <li className={styles.dropdown}>
-          <button className={styles.dropbtn}>Shop ▼</button>
+          <button className={styles.dropbtn}>
+            Shop <FaCaretDown size={14} />
+          </button>
+
           <div className={styles.dropdownContent}>
             <Link to="/shop/category1">Category 1</Link>
             <Link to="/shop/category2">Category 2</Link>
             <Link to="/shop/category3">Category 3</Link>
           </div>
         </li>
+
         <li>
           <Link to="/on-sale" className={styles.menuItem}>
             On Sale
