5af8e2f73c6e42cafe0c7ee369d542f130e579a1
giorgos <gkladouris@taxaki.com>
2025-12-05 16:16:04 +0200

    fix burger menu and add mobile friendly

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,90 +1,100 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { FaShoppingCart, FaUserCircle, FaCaretDown } from "react-icons/fa";
+import {
+  FaShoppingCart,
+  FaUserCircle,
+  FaCaretDown,
+  FaSearch,
+} from "react-icons/fa";
 import styles from "./Header.module.css";
 import Home from "../../pages/home/Home";
 import { slide as Menu } from "react-burger-menu";
 import "./Burger.css";
 
-const Navbar: React.FC = () => {
+const Header: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const closeMenu = () => setIsOpen(false);
 
   return (
-    <nav className={styles.navbar}>
-      {/* Mobile Burger Icon */}
-      <Menu isOpen={isOpen} onStateChange={(state) => setIsOpen(state.isOpen)}>
-        <Link onClick={closeMenu} to="/shop/category1">
-          Category 1
-        </Link>
-        <Link onClick={closeMenu} to="/shop/category2">
-          Category 2
-        </Link>
-        <Link onClick={closeMenu} to="/shop/category3">
-          Category 3
-        </Link>
-        <Link onClick={closeMenu} to="/on-sale">
-          On Sale
-        </Link>
-        <Link onClick={closeMenu} to="/new-arrivals">
-          New Arrivals
-        </Link>
-        <Link onClick={closeMenu} to="/brands">
-          Brands
-        </Link>
-      </Menu>
-
-      {/* Logo */}
-      <h1 className={styles.logo}>
-        <Link to={Home.route}>SHOP.CO</Link>
-      </h1>
-
-      {/* Desktop Menu */}
-      <ul className={styles.menu}>
-        <li className={styles.dropdown}>
-          <button className={styles.dropbtn}>
-            Shop <FaCaretDown size={14} />
-          </button>
-
-          <div className={styles.dropdownContent}>
-            <Link to="/shop/category1">Category 1</Link>
-            <Link to="/shop/category2">Category 2</Link>
-            <Link to="/shop/category3">Category 3</Link>
-          </div>
-        </li>
-
-        <li>
-          <Link to="/on-sale" className={styles.menuItem}>
+    <>
+      <div className={styles.header}>
+        <Menu
+          isOpen={isOpen}
+          onStateChange={(state) => setIsOpen(state.isOpen)}
+        >
+          <Link onClick={closeMenu} to="/shop/category1">
+            Category 1
+          </Link>
+          <Link onClick={closeMenu} to="/shop/category2">
+            Category 2
+          </Link>
+          <Link onClick={closeMenu} to="/shop/category3">
+            Category 3
+          </Link>
+          <Link onClick={closeMenu} to="/on-sale">
             On Sale
           </Link>
-        </li>
-        <li>
-          <Link to="/new-arrivals" className={styles.menuItem}>
+          <Link onClick={closeMenu} to="/new-arrivals">
             New Arrivals
           </Link>
-        </li>
-        <li>
-          <Link to="/brands" className={styles.menuItem}>
+          <Link onClick={closeMenu} to="/brands">
             Brands
           </Link>
-        </li>
-      </ul>
+        </Menu>
+        <nav className={styles.navbar}>
+          {/* Logo */}
+          <h1 className={styles.logo}>
+            <Link to={Home.route}>SHOP.CO</Link>
+          </h1>
 
-      {/* Search Bar */}
-      <input
-        type="text"
-        placeholder="Search for products..."
-        className={styles.searchBar}
-      />
+          {/* Desktop Menu */}
+          <ul className={styles.menu}>
+            <li className={styles.dropdown}>
+              <button className={styles.dropbtn}>
+                Shop <FaCaretDown size={14} />
+              </button>
 
-      {/* Icons */}
-      <div className={styles.icons}>
-        <FaShoppingCart size={20} />
-        <FaUserCircle size={20} />
+              <div className={styles.dropdownContent}>
+                <Link to="/shop/category1">Category 1</Link>
+                <Link to="/shop/category2">Category 2</Link>
+                <Link to="/shop/category3">Category 3</Link>
+              </div>
+            </li>
+
+            <li>
+              <Link to="/on-sale" className={styles.menuItem}>
+                On Sale
+              </Link>
+            </li>
+            <li>
+              <Link to="/new-arrivals" className={styles.menuItem}>
+                New Arrivals
+              </Link>
+            </li>
+            <li>
+              <Link to="/brands" className={styles.menuItem}>
+                Brands
+              </Link>
+            </li>
+          </ul>
+
+          <input
+            type="text"
+            placeholder="Search for products..."
+            className={styles.searchBar}
+          />
+
+          {/* Icons */}
+          <div className={styles.icons}>
+            <FaSearch size={20} className={styles.mobileSearchIcon} />
+            <FaShoppingCart size={20} />
+            <FaUserCircle size={20} />
+          </div>
+        </nav>
       </div>
-    </nav>
+    </>
   );
 };
 
-export default Navbar;
+export default Header;
